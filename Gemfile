a8640be59e766572dbe15d36ce646f2a48d21b1c
Jez Cope <j.cope@erambler.co.uk>
2013-02-23 17:21:49 +0000

    Initial database setup.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'thor'
+
+gem 'sequel'
+gem 'sqlite3'
+
 gem 'oai'
 gem 'libxml-ruby'
+
 gem 'andand'
---
+++ db/migrations/001_initial_setup.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  change do
+    create_table(:dois) do
+      primary_key :id
+      foreign_key :repository_id, :repositories
+      String :doi, null: false, index: true
+      String :url
+    end
+
+    create_table(:repositories) do
+      primary_key :id
+      String :base_url, null: false
+      String :name
+      String :admin_email
+      DateTime :earliest_datestamp
+    end
+  end
+end

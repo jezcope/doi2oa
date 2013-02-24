63443471749ae082cdb2f84cf2f8d5feafdf46bf
Jez Cope <J.Cope@bath.ac.uk>
2013-02-24 16:33:51 +0000

    Make rspec start with a blank database each time.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 require 'sequel'
+require 'sequel/extensions/migration'
 require 'factory_girl'
 
-Sequel::Model.db = Sequel.sqlite('tmp/test.sqlite')
+DB = Sequel.sqlite
+Sequel::Migrator.apply(DB, 'db/migrations')
+Sequel::Model.db = DB
 
 FactoryGirl.find_definitions

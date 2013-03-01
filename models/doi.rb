a2ee609b485b49c3a3c834968e35a27c2e00559c
Jez Cope <j.cope@erambler.co.uk>
2013-03-01 20:22:47 +0000

    Loosen up DOI validation.

--- models/doi.rb
+++ models/doi.rb
@@ -11,7 +11,7 @@ class Doi < Sequel::Model
 
     validates_presence [:repository, :doi]
     validates_format    %r{^https?://([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$}, :url
-    validates_format    %r{^10\.[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)?/[0-9A-Za-z][0-9A-Za-z./-]+$}, :doi
+    validates_format    %r{^10\.[0-9A-Za-z]+(?:\.[0-9A-Za-z]+)?/}, :doi
   end
 
   def self.new_from_oai_record(repository, record)
